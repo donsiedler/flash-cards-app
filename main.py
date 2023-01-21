eea9d0d81c035c4667f19f47ad778bde888df1a1
Artur Siedlerski <don.siedler@gmail.com>
2023-01-21 15:43:57 +0100

    add tracking progress: save words to learn in a separate csv file

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from tkinter import Tk, PhotoImage, Canvas, Button
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ------------------------ READ CSV DATA ------------------------- #
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    words_to_learn = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    with open("data/words_to_learn.csv", "w"):
+        # Creates a new csv file 'words_to_learn' if it doesn't exist
+        pass
+except pandas.errors.EmptyDataError:
+    words_to_learn = pandas.read_csv("data/french_words.csv")
+
+to_learn = words_to_learn.to_dict(orient="records")
+print(f"Words to learn: {len(to_learn)}")
 current_card = {}
 
 
@@ -14,17 +23,40 @@ current_card = {}
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card, image=card_front_img)
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-    flip_timer = window.after(3000, flip_card)
+    try:
+        current_card = random.choice(to_learn)
+    except IndexError:
+        canvas.itemconfig(card, image=card_front_img)
+        canvas.itemconfig(card_title, text="Congrats", fill="black")
+        canvas.itemconfig(card_word, text="No cards left!", fill="black")
+    else:
+        canvas.itemconfig(card, image=card_front_img)
+        canvas.itemconfig(card_title, text="French", fill="black")
+        canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+        flip_timer = window.after(3000, flip_card)
 
 
 def flip_card():
     canvas.itemconfig(card, image=card_back_img)
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
+# ----------------- ANSWERS AND SAVING PROGRESS ------------------ #
+
+def known_word():
+    next_card()
+    try:
+        to_learn.remove(current_card)
+    except ValueError:
+        return
+    else:
+        df = pandas.DataFrame(to_learn)
+        df.to_csv("data/words_to_learn.csv", index=False)
+
+
+def unknown_word():
+    next_card()
 
 
 # --------------------------- UI SETUP --------------------------- #
@@ -49,15 +81,13 @@ card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 # Buttons
-wrong_btn = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=next_card)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=unknown_word)
 wrong_btn.grid(row=2, column=1, pady=50)
 
-right_btn = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=next_card)
+right_btn = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=known_word)
 right_btn.grid(row=2, column=2, pady=50)
 
 # Show first card after running the app
 next_card()
 
-
 window.mainloop()
-
